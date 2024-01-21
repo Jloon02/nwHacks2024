01303217ada2c4ef5b547131a35148a93fc1c445
Josh Chung <jca472@sfu.ca>
2024-01-20 21:36:42 -0800

    Button Init Commit

---
+++ the-button/src/components/Button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    // TODO: Add a clickable button that changes colour
+    <div style={{mixBlendMode: 'darken', color: '#005500', fontSize: 120, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>PRESS</div>
+  )
+}
+
+export default Button
